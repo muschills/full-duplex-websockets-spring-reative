c3009c4fa393d8226d81830b78358068fe76d8d4
Musafili Chilembo <mchilembo@cellulant.com>
2021-08-25 15:51:05 +0200

    initial commit

---
+++ src/main/java/com/rogue1/fullduplex/FullDuplexWebSocketsApplication.java
@@ -0,0 +1,13 @@
+package com.rogue1.fullduplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullDuplexWebSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullDuplexWebSocketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogue1/fullduplex/FullDuplexWebSocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogue1.fullduplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullDuplexWebSocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
